65e4db26f40a0eaa57f9d5424bc2fc7a339edbd3
David Massana <00072416@uca.edu.sv>
2019-10-30 11:38:15 -0600

    Updates

---
+++ precache-manifest.435b8d78bb0876f3543f1dd3b5e5addf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ded04546b3e785051b847745270d603",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "9fd450ca36ee7705e6f8",
+    "url": "/tic-tac-toe/static/css/main.67d839ee.chunk.css"
+  },
+  {
+    "revision": "7144a41f7e6c783a7e97",
+    "url": "/tic-tac-toe/static/js/2.83a1039e.chunk.js"
+  },
+  {
+    "revision": "9fd450ca36ee7705e6f8",
+    "url": "/tic-tac-toe/static/js/main.7283700c.chunk.js"
+  },
+  {
+    "revision": "385629666264212be688",
+    "url": "/tic-tac-toe/static/js/runtime-main.9b00db00.js"
+  }
+]);
